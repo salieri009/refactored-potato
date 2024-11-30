5af90fb55d99f3cda9c15d3fe643da3d6fbe7d39
Salieri009 <kordalek@naver.com>
2024-11-30 12:34:13 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+//create a web server
+var http = require('http');
+var fs = require('fs');
+
+//create a server object:
+http.createServer(function (req, res) {
+  fs.readFile('comment.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); //the server object listens on port 8080g
